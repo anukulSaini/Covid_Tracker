d66564c40f5c20bdb37a9b9516cb7bbee3bad9b9
anukulSaini <anukulsaini2295@gmail.com>
2021-05-02 18:38:10 +0530

    chart show data in bar form

--- src/Components/Charts/Charts.jsx
+++ src/Components/Charts/Charts.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line,Bar} from 'react-chartjs-2';
+import styles from './Charts.module.css';
+
+
 
 const Chart = () => {
+
+    const [dailyData,setDailyData] = useState([]);
+
+  useEffect(()=>{
+
+   const fetchAPI = async () =>{
+        setDailyData (await fetchDailyData());
+      }
+      fetchAPI();
+  });
+ 
+
+  const lineChart =(
+      dailyData.length
+     ?(<Line
+     data={{
+        //  labels:dailyData.map(({date})=>date),
+        labels: dailyData.map(({ reportDate }) => new Date(reportDate).toDateString()),
+         datasets:[{
+             data:dailyData.map(({confirmed})=>confirmed),
+            // data: dailyData.map((data) => data.confirmed),
+             label:'Infected',
+             borderColor:'#3333ff',
+             fill:true
+         },{
+            data:dailyData.map(({deaths})=>deaths),
+            label:'Deaths',
+            borderColor:'red',
+            backgroundColor:'rgba(255,0,0,0.5)',
+            fill:true
+         }],
+     }}
+     />):null
+  );
+
+
     return ( 
-        <div>
-            <h1>
-                Charts
-            </h1>
+        <div className={styles.container}>
+            {lineChart}
         </div>
      );
 }
