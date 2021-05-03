caaa84a26af8a5437f8ffc69087f7d75fa81bebc
Anukul saini <anukulsaini2293@gmail.com>
2021-05-03 23:48:49 +0530

    fetching required data from api

--- src/api/index.js
+++ src/api/index.js
@@ -2,20 +2,44 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchdata = async() =>{
+export const fetchdata = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+     
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+export const fetchDailyData = async() =>{
     try{
-        const { data } = await axios.get(url);
-        
-        const modifiedresponse ={
-            confirmed:data.confirmed,
-            recovered:data.recovered,
-            deaths:data.deaths,
-            lastupdated:data.lastupdated
-        }
-        
-        return modifiedresponse;
+         const {data} = await axios.get(`${url}/daily`);
+         
+         const modifiedData = data.map((dailyData)=>({
+          confirmed:dailyData.confirmed.total,
+          deaths:dailyData.deaths.total,
+          reportDate:dailyData.reportDate,
+         }));
+         return modifiedData;
     }
-    catch (error){
-        
+    catch(error){
+
     }
+}
+
+export const fetchcountries = async()=>{
+    try{
+    const {data:{countries}} = await axios.get(`${url}/countries`);
+    return countries.map((country)=>country.name);
+    }
+    catch(error){
+
+    }
+    
 }
